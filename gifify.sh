743b84f9d9590b3a9d9cdb291c85db3377a825d8
Andrew Petersen <kirbysayshi@gmail.com>
2014-01-02 15:58:34 -0500

    provide -r option for increased (frame)rate

--- gifify.sh
+++ gifify.sh
@@ -8,6 +8,7 @@ function printHelpAndExit {
   echo '  c CROP:   The x and y crops, from the top left of the image, i.e. 640:480'
   echo '  o OUTPUT: The basename of the file to be output (default "output")'
   echo '  n:        Do not upload the resulting image to CloudApp'
+  echo '  r FPS:    Output at this (frame)rate (default 10)'
   echo '  x:        Remove the original file and resulting .gif once the script is complete'
   echo ''
   echo 'Example:'
@@ -17,14 +18,17 @@ function printHelpAndExit {
 
 noupload=0
 
+fps=10
+
 OPTERR=0
 
-while getopts "c:o:nx" opt; do
+while getopts "c:o:r:nx" opt; do
   case $opt in
     c) crop=$OPTARG;;
     h) printHelpAndExit 0;;
     o) output=$OPTARG;;
     n) noupload=1;;
+    r) fps=$OPTARG;;
     x) cleanup=1;;
     *) printHelpAndExit 1;;
   esac
@@ -46,7 +50,10 @@ else
   crop=
 fi
 
-ffmpeg -i $filename $crop -r 10 -f image2pipe -vcodec ppm - | convert -verbose +dither -layers Optimize - ${output}.gif
+# -delay uses time per tick and defaults to 100, so 60fps == -delay 1.666666
+delay=$(bc -l <<< "100/$fps")
+
+ffmpeg -i $filename $crop -r $fps -f image2pipe -vcodec ppm - | convert -verbose +dither -layers Optimize -delay $delay - ${output}.gif
 
 if [ $noupload -ne 1 ]; then
   echo `cloudapp -d ${output}.gif`
